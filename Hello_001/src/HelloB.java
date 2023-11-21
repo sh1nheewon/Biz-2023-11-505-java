747d7dcbec2b123b9bb2e6069afe22d5700566de
sh1nheewon <tlsgmldnjs00@naver.com>
2023-11-21 16:14:19 +0900

    java 프로젝트 시작

---
+++ Hello_001/src/HelloB.java
@@ -0,0 +1,20 @@
+/*
+ * 문장이 시작될 때 슬래시 2개로 시작되거나
+ * 슬래시 별표로 시작된 여러라인의 문장은 Comment 라고 한다.
+ * Comment 는 Java 가 무시한다. 
+ * 슬래시 2개로 시작되는 문장은 Comment 라고 한다.
+ */
+// Comment 문장은 Java 가 공백과 같이 취급한다.
+
+// 나의 첫번째 자바 파일
+public class HelloB {
+	
+	// 자바 파일에서 제일 중요한 시작 블럭
+	public static void main(String[] args) {
+		
+		// 안녕하세요 라는 문자열을 Console 에 출력하는 코드
+		System.out.println("안녕하세요");
+		
+	}
+	
+}
