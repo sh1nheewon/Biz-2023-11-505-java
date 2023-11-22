4bc27a47d45b1f49543268e0e1597bf35b361483
sh1nheewon <tlsgmldnjs00@naver.com>
2023-11-22 13:33:50 +0900

    오전수업

---
+++ Hello_002/src/com/callor/hello/var/VarA.java
@@ -0,0 +1,34 @@
+package com.callor.hello.var;
+
+public class VarA {
+	public static void main(String[] args) {
+		// 정수형변수 num1 을 선언하고 0으로 clear 
+		int num1 = 0;
+		int num2 = 0;
+		
+		int sum = 0;
+		/*
+		 * 변수 만들기
+		 * 1. 변수에 어떤 데이터를 보관할 것인가 정하기
+		 * 2. 변수에 보관할 데이터가 어떤 type 인가 살펴보기
+		 * 3. 보관할 데이터 type 의 키워드 확인(int, long)
+		 * 4. 변수 이름 짓기 
+		 *  가. 가급적 2개 이상의 단어를 조합하여 변수에 어떤 데이터가 담기고, 어떤 일에 쓰일지 이름만 보고 알 수 있도록 만든다
+		 *  나. 첫 글자는 반드기 영문 소문자. 
+		 *  다. 2개 이상의 단어를 조합 할 때는 두 번째 부터 각 단어의 첫 글자는 대문자, 나머지는 소문자로 작성
+		 *  (Camel case)
+		 *  라. 비슷한 유형의 변수가 있을때, 영문자+숫자 조합 
+		 */
+		int intKor = 0;
+		int intEng = 0;
+		int intTotal = 0;
+		
+		
+		int scoreKor = 0;
+		int scoreEng = 0;
+		int scoreMath = 0;
+		int scoreMusic = 0;
+		int scoreArt = 0;
+		
+	}
+}
