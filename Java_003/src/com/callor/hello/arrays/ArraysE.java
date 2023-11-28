232b0d81eaa2500d6505c2037e54b3927ad23a93
sh1nheewon <tlsgmldnjs00@naver.com>
2023-11-28 16:37:28 +0900

    Arrays

---
+++ Java_003/src/com/callor/hello/arrays/ArraysE.java
@@ -0,0 +1,15 @@
+package com.callor.hello.arrays;
+
+public class ArraysE {
+	public static void main(String[] args) {
+		int[] nums = new int[10];
+		for (int i = 0; i < 10; i++) {
+			nums[i] = (int) (Math.random() * 50) + 51;
+		}
+
+		for (int i = 0; i < 10; i++) {
+			if (nums[i] > 80)
+				System.out.printf("index %d 번 요소의 값 : %d \n", i, nums[i]);
+		}
+	}
+}
