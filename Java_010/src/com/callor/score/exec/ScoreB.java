5ca59ea485f554c04baba3745fad42ab3738c338
sh1nheewon <tlsgmldnjs00@naver.com>
2023-12-07 11:50:27 +0900

    Score

---
+++ Java_010/src/com/callor/score/exec/ScoreB.java
@@ -0,0 +1,22 @@
+package com.callor.score.exec;
+
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.NumberService;
+
+public class ScoreB {
+	public static void main(String[] args) {
+		NumberService numberService = new NumberService();
+		int num1 = numberService.inputNumber("국어 점수를",50,100);
+		int num2 = numberService.inputNumber("영어 점수를",50,100);
+		int num3 = numberService.inputNumber("수학 점수를",50,100);
+		
+		ScoreDto scoreDto = new ScoreDto();
+		
+		scoreDto.kor = num1;
+		scoreDto.eng = num2;
+		scoreDto.math = num3; 
+		
+		System.out.printf("총점 : %d",scoreDto.getTotal());
+				
+	}
+}
